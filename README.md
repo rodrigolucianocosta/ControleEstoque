9e2c776b6feca819beae20e2b67de1819ad9b55b
root <root@r-one.r-one-server>
2016-05-30 12:03:09 -0300

    modificação de base de SQLite para MySQL

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ControleEstoque
 controle de estoque web
+
+ problema encontrado: Mudanca da base de SQLite para mysql
+
+solução
+Upgrade pip version
+sudo pip install pip --upgrade
+
+Construção das dependencias para python-mysqldb libraries
+sudo apt-get build dep python-mysqldb
+
+Instalação Python Mysql Libraires
+sudo pip install MySQl-python
+
+criação da database controleEstoque01
+
+create user 'usuario' by 'password'
+
+grant all  on nomebase.* to usuario'@'%';
+
+flush privileges; 
+
--- rOne/rOne/settings.py
+++ rOne/rOne/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'rOne.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': os.path.join(BASE_DIR, 'db.mysql'),
+	'NAME' : 'controleEstoque01',
+	'USER' : 'rodrigo',
+	'PASSWORD' : 'marvel01',
+	'HOST' : 'localhost',
+	'PORT' : '3306'
+	
+     }
+
 }
 
 
